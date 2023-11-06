24f57204eee3c2c2c652342782d22e4076239c71
Patrick Brumund <patrickbrumund@gmail.com>
2023-11-06 23:21:18 +0100

    updated to include other variables

--- gp/fileloading.py
+++ gp/fileloading.py
@@ -2,7 +2,7 @@ import numpy as np
 import datetime
 import csv
 
-def load_prediction(start_time, end_time):
+def load_forecast(start_time, end_time):
     """Loads numerical weather predictions from a csv file
     
     Keyword arguments:
@@ -10,20 +10,36 @@ def load_prediction(start_time, end_time):
     end_time -- end of the time window for predictions, second part of file name
     Returns times and predictions for time windows of 0-6, 6-12 and 12-18 h after release of newest weather forecast
     """
-    filename = start_time.strftime("%Y%m%d") + "-" + end_time.strftime("%Y%m%d") + "_predictions.csv"
+    # filename = start_time.strftime("%Y%m%d") + "-" + end_time.strftime("%Y%m%d") + "_predictions.csv"
+    # with open(filename, mode="r") as f:
+    #     reader = csv.reader(f, delimiter=';')
+    #     table = np.array(list(reader))
+    #     times_sh = table[:,0].astype(float)
+    #     times_sh = np.asarray(times_sh, dtype='datetime64[s]')
+    #     wind_forecast_sh = table[:,1].astype(float)
+    #     times_mh = table[:,2].astype(float)
+    #     times_mh = np.asarray(times_mh, dtype='datetime64[s]')
+    #     wind_forecast_mh = table[:,3].astype(float)
+    #     times_lh = table[:,4].astype(float)
+    #     times_lh = np.asarray(times_mh, dtype='datetime64[s]')
+    #     wind_forecast_lh = table[:,5].astype(float)
+    # return times_sh, wind_forecast_sh, times_mh, wind_forecast_mh, times_lh, wind_forecast_lh
+    filename = start_time.strftime("%Y%m%d") + "-" + end_time.strftime("%Y%m%d") + "_forecast.csv"
+    prediction_dict = {}
     with open(filename, mode="r") as f:
         reader = csv.reader(f, delimiter=';')
+        headers = next(reader)
         table = np.array(list(reader))
-        times_sh = table[:,0].astype(float)
-        times_sh = np.asarray(times_sh, dtype='datetime64[s]')
-        wind_forecast_sh = table[:,1].astype(float)
-        times_mh = table[:,2].astype(float)
-        times_mh = np.asarray(times_mh, dtype='datetime64[s]')
-        wind_forecast_mh = table[:,3].astype(float)
-        times_lh = table[:,4].astype(float)
-        times_lh = np.asarray(times_mh, dtype='datetime64[s]')
-        wind_forecast_lh = table[:,5].astype(float)
-    return times_sh, wind_forecast_sh, times_mh, wind_forecast_mh, times_lh, wind_forecast_lh
+        for i, h in enumerate(headers):
+            column_i = table[:,i]
+            if "times" in h:
+                column_i = np.asarray(column_i.astype(float), dtype='datetime64[s]')
+            else:
+                column_i = column_i.astype(float)
+            prediction_dict[h] = column_i
+        return prediction_dict
+
+
 
 def load_weather_measurements(start_time, end_time):
     """Loads wind measurements from a csv file
@@ -44,9 +60,18 @@ def load_weather_measurements(start_time, end_time):
 
 def load_weather_data(start_time, end_time):
     """Loads NWP and measurements, returns them as a dictionary"""
-    times_sh, wind_forecast_sh, times_mh, wind_forecast_mh, times_lh, wind_forecast_lh = load_prediction(start_time, end_time)
     times_obs, wind_speed_obs = load_weather_measurements(start_time, end_time)
-    c_correction = np.mean(wind_speed_obs)/np.mean(wind_forecast_sh)
+    predictions = load_forecast(start_time, end_time)
+    wind_speeds_forecast = np.array([])
+    for key in predictions:
+        if "wind_speed" in key:
+            wind_speeds_forecast = np.append(wind_speeds_forecast, predictions[key])
+    c_correction = np.mean(wind_speed_obs)/np.mean(wind_speeds_forecast)
+    for key in predictions:
+        if "wind_speed" in key:
+            predictions[key] *= c_correction
+    # times_sh, wind_forecast_sh, times_mh, wind_forecast_mh, times_lh, wind_forecast_lh = load_prediction(start_time, end_time)
+    # c_correction = np.mean(wind_speed_obs)/np.mean(wind_forecast_sh)
     times_interp = np.array([times_obs[0]+i*np.timedelta64(10, 'm') 
                              for i in range(int((times_obs[-1].astype(float)-times_obs[0].astype(float))//600000))])
     times_interp_datetime = np.array([times_obs[0].astype(datetime.datetime)+i*datetime.timedelta(minutes=10) 
@@ -56,12 +81,7 @@ def load_weather_data(start_time, end_time):
                                 (times_interp[-1].astype(float)-times_interp[0].astype(float)), 
                                 (times_obs.astype(float)-times_obs[0].astype(float))/
                                 (times_obs[-1].astype(float)-times_obs[0].astype(float)), wind_speed_obs)   # Interpolate to fill missing values
-    data = {'times_sh': times_sh,
-            'pred_sh':  c_correction*wind_forecast_sh,
-            'times_mh': times_mh,
-            'pred_mh':  c_correction*wind_forecast_mh,
-            'times_lh': times_lh,
-            'pred_lh':  c_correction*wind_forecast_lh,
-            'times_meas': times_interp_datetime,
-            'meas':     wind_obs_interp}
+    data = {'times_meas': times_interp_datetime,
+            'meas': wind_obs_interp}
+    data.update(predictions)
     return data
--- gp/utils.py
+++ gp/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import datetime
 
-def get_NWP(wind_table, time, steps):
+def get_NWP(wind_table, time, steps=0, key="wind_speed_10m"):
     """Interpolates weather prediction from dict using current forecast for the given time
     
     Uses linear interpolation to get values between full hours
@@ -9,20 +9,25 @@ def get_NWP(wind_table, time, steps):
     wind_table -- dictionary of NWP and measurements
     time -- time at current time step
     steps -- number of steps to predict ahead
+    key -- string with name of value as in dict, standard: "wind_speed_10m"
     """
     r = time.hour % 6
-    dt = time - wind_table['times_sh'][0].astype(datetime.datetime)
+    if key in ["wind_speed_10m", "wind_direction_10m", "air_pressure_at_sea_level", "air_temperature_2m"]:   # MET post-processed
+        times_sh = wind_table["times1_sh"]
+    else:
+        times_sh = wind_table['times2_sh']
+    dt = time - times_sh[0].astype(datetime.datetime)
     i_start = int(dt.total_seconds()//21600) * 6 #6 hours, time of last released forecast
-    predicted_trajectory = np.concatenate([wind_table['pred_sh'][i_start:i_start+6], 
-                                           wind_table['pred_mh'][i_start:i_start+6], 
-                                           wind_table['pred_lh'][i_start:]])    # Reconstruct the most recent NWP at the given time
+    predicted_trajectory = np.concatenate([wind_table[key+"_sh"][i_start:i_start+6], 
+                                           wind_table[key+"_mh"][i_start:i_start+6], 
+                                           wind_table[key+"_lh"][i_start:]])    # Reconstruct the most recent NWP at the given time
     
     i = (6*r + time.minute//10 + steps)//6
     wind_interp = (predicted_trajectory[i+1]*(time+steps*datetime.timedelta(minutes=10)).minute/60 
                    + predicted_trajectory[i]*(1-(time+steps*datetime.timedelta(minutes=10)).minute/60))
     return wind_interp
 
-def get_wind_value(wind_table, time, steps):
+def get_wind_value(wind_table, time, steps=0):
     """Returns measured wind speed, inputs as in get_NWP"""
     dt = time - wind_table['times_meas'][0]
     i = int(dt.total_seconds()/600) + steps
