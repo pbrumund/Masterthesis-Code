e6a0bb532a731c09d25bf4c660a221afa4e17fca
Patrick Brumund <patrickbrumund@gmail.com>
2023-11-06 23:21:48 +0100

    updated to include other variables

--- gp/get_historic_forecast.py
+++ gp/get_historic_forecast.py
@@ -15,7 +15,7 @@ lat = 61.21444
 lon = 2.27222
 # date range
 start_time = datetime.datetime(2020,1,1)
-end_time = datetime.datetime(2022,12,31)
+end_time = datetime.datetime(2020,1,31)
 # variables of interest
 variables_1 = ["wind_speed_10m", "wind_direction_10m", "air_pressure_at_sea_level", "air_temperature_2m"]   # MET post-processed
 variables_2 = ["specific_convective_available_potential_energy", "atmosphere_convective_inhibition"]    # MEPS
@@ -29,15 +29,16 @@ for v in ["times1"]+variables_1+["times2"]+variables_2:
 with open(filename_save, "w", newline='') as f:
     writer = csv.writer(f, delimiter=';')
     writer.writerow(headers)
-setup = True #only need to find grid index once
-nerrors = 0
+setup1 = True #only need to find grid index once
+setup2 = True
+# nerrors = 0
 
-times_1 = np.ma.array([]) #forecast for next 6 hours
-times_2 = np.ma.array([])  #forecast between 6 and 12 hours
-times_3 = np.ma.array([])  #forecast between 12 and 18 hours
-wind_speeds_1 = np.ma.array([])
-wind_speeds_2 = np.ma.array([])
-wind_speeds_3= np.ma.array([])
+# times_1 = np.ma.array([]) #forecast for next 6 hours
+# times_2 = np.ma.array([])  #forecast between 6 and 12 hours
+# times_3 = np.ma.array([])  #forecast between 12 and 18 hours
+# wind_speeds_1 = np.ma.array([])
+# wind_speeds_2 = np.ma.array([])
+# wind_speeds_3= np.ma.array([])
 
 time = start_time
 while(time < end_time):
@@ -48,12 +49,12 @@ while(time < end_time):
     day = time.day
     hour = time.hour
 
-    filename1 = f"https://thredds.met.no/thredds/dodsC/metpparchive/{year}/{month:02}/{day:02}/met_forecast_1_0km_nordic_{year}{month:02}{day:02}T{hour:02}Z.nc"
-    filename2 = f"https://thredds.met.no/thredds/dodsC/meps25epsarchive/{year}/{month:02}/{day:02}/meps_mbr0_full_2_5km_{year}{month:02}{day:02}T{hour:02}Z.nc"
+    # MET post-processed
     try:
-        ncfile1   = netCDF4.Dataset(filename1,"r")
-        ncfile2  = netCDF4.Dataset(filename2,"r")
-        if setup:
+        filename = f"https://thredds.met.no/thredds/dodsC/metpparchive/{year}/{month:02}/{day:02}/met_forecast_1_0km_nordic_{year}{month:02}{day:02}T{hour:02}Z.nc"
+        ncfile   = netCDF4.Dataset(filename,"r")
+
+        if setup1:
             # Get indices of closest grid point to coordinates on first iteration
             # Code adapted from https://github.com/metno/NWPdocs/wiki/Examples
             crs = pyproj.CRS.from_cf(
@@ -73,41 +74,79 @@ while(time < end_time):
             X,Y = proj.transform(lon,lat)
 
             # Find nearest neighbour
-            x1 = ncfile1.variables["x"][:]
-            y1 = ncfile1.variables["y"][:]
+            x = ncfile.variables["x"][:]
+            y = ncfile.variables["y"][:]
 
-            Ix1 = np.argmin(np.abs(x1 - X))
-            Iy1 = np.argmin(np.abs(y1 - Y))
+            Ix1 = np.argmin(np.abs(x - X))
+            Iy1 = np.argmin(np.abs(y - Y))
 
-            x2 = ncfile2.variables["x"][:]
-            y2 = ncfile2.variables["y"][:]
-
-            Ix2 = np.argmin(np.abs(x2 - X))
-            Iy2 = np.argmin(np.abs(y2 - Y))
-            setup = False
-
+            setup1 = False
         predictions_i = {}
-        predictions_i['times1'] = ncfile1.variables["time"][:]
+        predictions_i['times1'] = ncfile.variables["time"][:]
         for variable in variables_1:
-            predictions_i[variable] = ncfile1.variables[variable][:,Iy1,Ix1]
-        predictions_i['times2'] = ncfile2.variables["time"][:]
-        for variable in variables_2:
-            predictions_i[variable] = ncfile2.variables[variable][:,0,Iy2,Ix2]
-        # wind_speeds_i = ncfile.variables["wind_speed_10m"][:,Iy,Ix]
-        # times2_i = ncfile2.variables["time"][:]
-        # cape_i = ncfile2.variables["specific_convective_available_potential_energy"][:,Iy2,Ix2]
-        nerrors = 0
+            predictions_i[variable] = ncfile.variables[variable][:,Iy1,Ix1]
     except:
-        # missing data on server, use last prediction
         with open("missing_dates.txt", 'a') as errorfile:
-            errorfile.write(time.strftime("%d.%m.%Y. %H:%M\n"))
-        nerrors += 1
-        for key in predictions_i:
+            errorfile.write(time.strftime("%d.%m.%Y. %H:%M: post-processed data missing\n"))
+            print('Post-processed data missing')
+        for key in variables_1 + ["times1"]:
             predictions_i[key] = predictions_i[key][6:]
-        # times_i = times_i[6:]
-        # wind_speeds_i = wind_speeds_i[6:]
-        # times2_i = times2_i[6:]
-        # cape_i = cape_i[6:]       
+
+    # MEPS
+    try:
+        filename = f"https://thredds.met.no/thredds/dodsC/meps25epsarchive/{year}/{month:02}/{day:02}/meps_mbr0_full_2_5km_{year}{month:02}{day:02}T{hour:02}Z.nc"
+        ncfile   = netCDF4.Dataset(filename,"r")
+
+        if setup2:
+            # Get indices of closest grid point to coordinates on first iteration
+            # Code adapted from https://github.com/metno/NWPdocs/wiki/Examples
+            crs = pyproj.CRS.from_cf(
+                {
+                    "grid_mapping_name": "lambert_conformal_conic",
+                    "standard_parallel": [63.3, 63.3],
+                    "longitude_of_central_meridian": 15.0,
+                    "latitude_of_projection_origin": 63.3,
+                    "earth_radius": 6371000.0,
+                }
+            )
+            # Transformer to project from ESPG:4368 (WGS:84) to our lambert_conformal_conic
+        
+            proj = pyproj.Proj.from_crs(4326, crs, always_xy=True)
+            # Compute projected coordinates of lat/lon point
+
+            X,Y = proj.transform(lon,lat)
+
+            # Find nearest neighbour
+            x = ncfile.variables["x"][:]
+            y = ncfile.variables["y"][:]
+
+            Ix2 = np.argmin(np.abs(x - X))
+            Iy2 = np.argmin(np.abs(y - Y))
+
+            setup2 = False
+        predictions_i['times2'] = ncfile.variables["time"][:]
+        for variable in variables_2:
+            predictions_i[variable] = ncfile.variables[variable][:,0,Iy2,Ix2]
+    except:
+        try:
+            filename = f"https://thredds.met.no/thredds/dodsC/meps25epsarchive/{year}/{month:02}/{day:02}/meps_mbr1_full_2_5km_{year}{month:02}{day:02}T{hour:02}Z.nc"
+            ncfile   = netCDF4.Dataset(filename,"r")
+            predictions_i['times2'] = ncfile.variables["time"][:]
+            for variable in variables_2:
+                predictions_i[variable] = ncfile.variables[variable][:,0,Iy2,Ix2]
+        except:
+            try:
+                filename = f"https://thredds.met.no/thredds/dodsC/meps25epsarchive/{year}/{month:02}/{day:02}/meps_lagged_6_h_subset_2_5km_{year}{month:02}{day:02}T{hour:02}Z.nc"
+                ncfile   = netCDF4.Dataset(filename,"r")
+                predictions_i['times2'] = ncfile.variables["time"][:]
+                for variable in variables_2:
+                    predictions_i[variable] = ncfile.variables[variable][:,0,Iy2,Ix2]
+            except:
+                with open("missing_dates.txt", 'a') as errorfile:
+                    errorfile.write(time.strftime("%d.%m.%Y. %H:%M: MEPS data missing\n"))
+                    print('MEPS data missing')
+                for key in variables_1 + ["times1"]:
+                    predictions_i[key] = predictions_i[key][6:]
     finally:
         if save:
             with open(filename_save, 'a') as file:
@@ -115,11 +154,85 @@ while(time < end_time):
                 for key in predictions_i:
                     v = predictions_i[key]
                     v1, v2, v3 = v[:6], v[6:12], v[12:18]
-                    v1, v2, v3 = np.ma.reshape(v1,6), np.ma.reshape(v2,6), np.ma.reshape(v3,6)
+                    v1, v2, v3 = np.ma.resize(v1,6), np.ma.resize(v2,6), np.ma.resize(v3,6)
                     v_list.extend([v1,v2,v3])
                 v_array = np.array(v_list).T
                 np.savetxt(file, v_array, delimiter=';', fmt='%s')
-        time = time + datetime.timedelta(hours=6)
+        time += datetime.timedelta(hours=6)
+
+    # filename1 = f"https://thredds.met.no/thredds/dodsC/metpparchive/{year}/{month:02}/{day:02}/met_forecast_1_0km_nordic_{year}{month:02}{day:02}T{hour:02}Z.nc"
+    # filename2 = f"https://thredds.met.no/thredds/dodsC/meps25epsarchive/{year}/{month:02}/{day:02}/meps_mbr0_full_2_5km_{year}{month:02}{day:02}T{hour:02}Z.nc"
+    # try:
+    #     ncfile1   = netCDF4.Dataset(filename1,"r")
+    #     ncfile2  = netCDF4.Dataset(filename2,"r")
+    #     if setup:
+    #         # Get indices of closest grid point to coordinates on first iteration
+    #         # Code adapted from https://github.com/metno/NWPdocs/wiki/Examples
+    #         crs = pyproj.CRS.from_cf(
+    #             {
+    #                 "grid_mapping_name": "lambert_conformal_conic",
+    #                 "standard_parallel": [63.3, 63.3],
+    #                 "longitude_of_central_meridian": 15.0,
+    #                 "latitude_of_projection_origin": 63.3,
+    #                 "earth_radius": 6371000.0,
+    #             }
+    #         )
+    #         # Transformer to project from ESPG:4368 (WGS:84) to our lambert_conformal_conic
+        
+    #         proj = pyproj.Proj.from_crs(4326, crs, always_xy=True)
+    #         # Compute projected coordinates of lat/lon point
+
+    #         X,Y = proj.transform(lon,lat)
+
+    #         # Find nearest neighbour
+    #         x1 = ncfile1.variables["x"][:]
+    #         y1 = ncfile1.variables["y"][:]
+
+    #         Ix1 = np.argmin(np.abs(x1 - X))
+    #         Iy1 = np.argmin(np.abs(y1 - Y))
+
+    #         x2 = ncfile2.variables["x"][:]
+    #         y2 = ncfile2.variables["y"][:]
+
+    #         Ix2 = np.argmin(np.abs(x2 - X))
+    #         Iy2 = np.argmin(np.abs(y2 - Y))
+    #         setup = False
+
+    #     predictions_i = {}
+    #     predictions_i['times1'] = ncfile1.variables["time"][:]
+    #     for variable in variables_1:
+    #         predictions_i[variable] = ncfile1.variables[variable][:,Iy1,Ix1]
+    #     predictions_i['times2'] = ncfile2.variables["time"][:]
+    #     for variable in variables_2:
+    #         predictions_i[variable] = ncfile2.variables[variable][:,0,Iy2,Ix2]
+    #     # wind_speeds_i = ncfile.variables["wind_speed_10m"][:,Iy,Ix]
+    #     # times2_i = ncfile2.variables["time"][:]
+    #     # cape_i = ncfile2.variables["specific_convective_available_potential_energy"][:,Iy2,Ix2]
+    #     nerrors = 0
+    # except:
+    #     # missing data on server, use last prediction
+    #     with open("missing_dates.txt", 'a') as errorfile:
+    #         errorfile.write(time.strftime("%d.%m.%Y. %H:%M\n"))
+    #     nerrors += 1
+    #     for key in predictions_i:
+    #         predictions_i[key] = predictions_i[key][6:]
+    #     print("missing data")
+    #     # times_i = times_i[6:]
+    #     # wind_speeds_i = wind_speeds_i[6:]
+    #     # times2_i = times2_i[6:]
+    #     # cape_i = cape_i[6:]       
+    # finally:
+    #     if save:
+    #         with open(filename_save, 'a') as file:
+    #             v_list = []
+    #             for key in predictions_i:
+    #                 v = predictions_i[key]
+    #                 v1, v2, v3 = v[:6], v[6:12], v[12:18]
+    #                 v1, v2, v3 = np.ma.resize(v1,6), np.ma.resize(v2,6), np.ma.resize(v3,6)
+    #                 v_list.extend([v1,v2,v3])
+    #             v_array = np.array(v_list).T
+    #             np.savetxt(file, v_array, delimiter=';', fmt='%s')
+    #     time = time + datetime.timedelta(hours=6)
                 # t1, t2, t3 = times_i[:6], times_i[6:12], times_i[12:18]
                 # w1, w2, w3 = wind_speeds_i[:6], wind_speeds_i[6:12], wind_speeds_i[12:18]
                 # np.savetxt(file, np.array([np.ma.resize(t1,6),np.ma.resize(w1,6),np.ma.resize(t2,6),np.ma.resize(w2,6),np.ma.resize(t3,6),np.ma.resize(w3,6)]).T, delimiter=';', fmt='%s')
---
+++ gp/get_historic_wind_data.py
@@ -0,0 +1,44 @@
+# Adapted from https://frost.met.no/python_example.html
+import requests
+import datetime
+import pandas as pd
+import numpy as np
+
+fp = "../Winddaten/Frost_API_credentials.txt"
+with open(fp) as f:
+    client_id = f.readline().rstrip('\n')
+print(client_id)
+
+start_time = datetime.datetime(2020,1,1)
+end_time = datetime.datetime(2020,1,31)
+
+filename_save = start_time.strftime("%Y%m%d") + "-" + end_time.strftime("%Y%m%d") + "_observations.csv"
+
+end_time = end_time + datetime.timedelta(days=1)
+
+time = start_time
+while(time < end_time):
+    print(time)
+    next_day = time + datetime.timedelta(days=1)
+    endpoint = 'https://frost.met.no/observations/v0.jsonld'
+    parameters = {
+        'sources': 'SN76923',
+        'elements': 'mean(wind_speed PT1M),wind_speed',
+        'referencetime': time.strftime('%Y-%m-%d/')+next_day.strftime('%Y-%m-%d'),
+    }
+    # Issue an HTTP GET request
+    r = requests.get(endpoint, parameters, auth=(client_id,''))
+    # Extract JSON data
+    json = r.json()
+
+    if r.status_code == 200:
+        data = json['data']
+        with open(filename_save, "a") as f:
+            for x in data:
+                np.savetxt(f, np.array([[x['referenceTime'], x['observations'][0]['value']]]), delimiter=';', fmt='%s')
+    else:
+        print('Error! Returned status code %s' % r.status_code)
+        print('Message: %s' % json['error']['message'])
+        print('Reason: %s' % json['error']['reason'])
+        
+    time += datetime.timedelta(days=1)
